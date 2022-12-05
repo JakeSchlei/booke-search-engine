198a68a18d2084aab782816ca12fb19102d3816e
JakeSchlei <jakeschlei@gmail.com>
2022-12-05 13:37:16 -0800

    deleted routes and controller folder

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 const {typeDefs, resolvers} = require("./schemas");
 
 const db = require('./config/connection');
-const {authMiddleware} = require('/utils/auth');
+const {authMiddleware} = require('./utils/auth');
 
 const server = new ApolloServer({
   typeDefs,
